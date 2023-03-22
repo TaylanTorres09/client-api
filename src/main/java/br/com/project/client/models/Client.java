959936f3dd605dfc9725a494002c1e6d5972fea1
TaylanTorres09 <taylantorres@gmail.com>
2023-03-21 22:41:22 -0300

    Set default constructor to client

--- src/main/java/br/com/project/client/models/Client.java
+++ src/main/java/br/com/project/client/models/Client.java
@@ -7,10 +7,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "clients")
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Client {
     
