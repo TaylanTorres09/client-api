f4aae28e9ea4b49da7d30cf7e97877044cad6a48
TaylanTorres09 <taylantorres@gmail.com>
2023-03-22 12:57:03 -0300

    Method and endpoint to create Client

--- src/main/java/br/com/project/client/controllers/ClientController.java
+++ src/main/java/br/com/project/client/controllers/ClientController.java
@@ -1,13 +1,22 @@
 package br.com.project.client.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.project.client.dtos.ClientDTO;
 import br.com.project.client.models.Client;
 import br.com.project.client.services.ClientService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/client")
@@ -19,6 +28,15 @@ public class ClientController {
     @GetMapping("/{clientId}")
     public Client findByIdClient(@PathVariable Long clientId) {
         return clientService.findByIdClient(clientId);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Client> createClient(@Valid @RequestBody ClientDTO clientDTO) {
+        Client client = clientService.createClient(clientDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path(String.format("/client/%d", client.getId())).buildAndExpand(client.getId()).toUri();
+        
+        //return uri in headers
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/br/com/project/client/services/ClientService.java
+++ src/main/java/br/com/project/client/services/ClientService.java
@@ -1,8 +1,10 @@
 package br.com.project.client.services;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.project.client.dtos.ClientDTO;
 import br.com.project.client.models.Client;
 import br.com.project.client.repositories.ClientRepository;
 import br.com.project.client.services.exceptions.ObjectNotFound;
@@ -15,5 +17,11 @@ public class ClientService {
 
     public Client findByIdClient(Long clientId) {
         return clientRepository.findById(clientId).orElseThrow(() -> new ObjectNotFound("Client not found"));
+    }
+
+    public Client createClient(ClientDTO clientDTO) {
+        Client client = new Client();
+        BeanUtils.copyProperties(clientDTO, client);
+        return clientRepository.save(client);
     }
 }
